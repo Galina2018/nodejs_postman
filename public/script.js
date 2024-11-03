8497bcac8d5a0d871c4368de880d9193dbf142ba
Galina Gatalskaya <galinagatalya@gmail.com>
2024-11-03 23:45:42 +0300

    ui, server
    ui, server

--- public/script.js
+++ public/script.js
@@ -1,11 +1,8 @@
 getPage();
 
-function test() {
-  console.log('in test');
-}
 async function getReqs() {
   const response = await fetch('/getReqs', {
-    method: 'GET',
+    method: 'POST',
     headers: {
       Accept: 'application/json',
     },
@@ -13,22 +10,49 @@ async function getReqs() {
   return await response.json();
 }
 
+function sendR(data, evt) {
+  evt.preventDefault();
+  console.log('in sendR', data);
+}
+
+function setReqCur(req) {
+  const select = document.getElementById('select');
+  select.options[select.selectedIndex].text = req.method
+  const urlCur = document.getElementById('url');
+  urlCur.value = req.url
+  const bodyCur = document.getElementById('bodyReq')
+  bodyCur.value = req.body ? JSON.stringify(req.body) :''
+}
+
+async function sendRequest(form) {  
+  form.action = '/sendReq'
+  form.method = 'post'
+  console.log('form', form)
+  form.submit();
+// const r = await fetch('/sendReq',{method:'post'})
+// console.log('rrrr', r)
+}
+
 async function getPage() {
   const reqsArr = await getReqs();
   console.log(222, reqsArr);
   let reqs = document.getElementById('reqs');
-  let headers = document.getElementById('headers');
-
-  reqsArr.forEach((e) => {
-    if (e.id) {
-      reqs.innerHTML += `<div><button onclick="test()" class="ta-left w100">Метод: ${e.method}<br />${e.url}</button></div>`;
+  // let headers = document.getElementById('headers');
+  const arr = await reqsArr;
+  arr.forEach((req) => {
+    if (req.id) {
+      let req_ = JSON.stringify(req)
+      reqs.innerHTML += `<button onclick='setReqCur(${req_})' id="${req.id}" class="ta-left w100">Метод: ${req.method}<br />${
+        req.url
+      }</button>`;
     }
   });
-  
+
   // const form = document.getElementById('form');
   // form.addEventListener('submit', sendRequest);
   // async function sendRequest(evt) {
   //   evt.preventDefault();
+  //   console.log('in sendR******************************************');
   //   await fetch('/sendReq', {
   //     method: 'POST',
   //     body: new FormData(form),
--- server.js
+++ server.js
@@ -2,21 +2,22 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const multer = require('multer');
+const { randomUUID } = require('crypto');
 
 const webserver = express();
 const upload = multer();
 const port = 7380;
+// const resultFN = path.resolve(__dirname, 'result.json', 'utf8');
 
 webserver.use(express.urlencoded({ extended: true }));
 webserver.use(express.static(path.resolve(__dirname, 'public')));
 
 webserver.get('/', (req, res) => {
-  // console.log('get');
   const html = fs.readFileSync(path.resolve(__dirname, 'index.html'), 'utf8');
   res.send(html);
 });
 
-webserver.get('/getReqs', (req, res) => {
+webserver.post('/getReqs', (req, res) => {
   const reqs = fs.readFileSync(
     path.resolve(__dirname, './public', 'reqs.json'),
     'utf8'
@@ -24,18 +25,37 @@ webserver.get('/getReqs', (req, res) => {
   res.send(reqs);
 });
 
+webserver.post('/', upload.none(), (req, res) => {
+  let dataJson = fs.readFileSync(
+    path.resolve(__dirname, './public', 'reqs.json'),
+    'utf8'
+  );
+  let data = JSON.parse(dataJson);
+  if (req.body && req.body.method && req.body.url) {
+    req.body.id = randomUUID();
+    data.push(req.body);
+  }
+  fs.writeFileSync(
+    path.resolve(__dirname, './public', 'reqs.json'),
+    JSON.stringify(data),
+    'utf8'
+  );
+  res.redirect('/');
+});
+
 webserver.post('/sendReq', async (req, res) => {
   const body = req.body;
-  console.log('body:', body);
-  const response = await fetch(body.url, { method: body.method, 
-  //   headers: {
-  //   ContentType: 'text/html'
-  // }
- });
-  const html = await response
-  console.log(html)
-  res.setHeader('Content-Type','text/html')
-  res.send(html)
+  const response = await fetch(`${body.url}`, {
+    method: `${body.method}`,
+  });
+
+  // fs.writeFileSync(resultFN, result);
+
+  res.send({
+    status: response.status,
+    headers:  response.headers,
+    body: await response.text(),
+  });
 });
 
 webserver.listen(port, () => {
