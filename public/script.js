2bb0d13043c81466c11fd9df6937436340c2fde8
Galina Gatalskaya <galinagatalya@gmail.com>
2024-11-20 12:47:12 +0300

    ui, server
    ui, server

--- public/script.js
+++ public/script.js
@@ -1,5 +1,4 @@
-getPage();
-
+setReqCur(1)
 async function getReqs() {
   const response = await fetch('/getReqs', {
     method: 'POST',
@@ -7,7 +6,31 @@ async function getReqs() {
       Accept: 'application/json',
     },
   });
-  return await response.json();  
+  return await response.json();
+}
+
+async function saveReqs(reqs) {
+  console.log('saveReqs()', reqs)
+ await fetch('/saveReqs', {
+   method: "POST",
+   headers: {
+    'Content-Type': 'application/json',
+},
+   body: JSON.stringify(reqs)
+ })
+}
+
+async function saveReq(form) {
+  console.log('ffoorrmm', form)
+  await fetch('/saveReq', {
+    method: "POST",
+    headers: {
+      // 'Content-Type': 'application/json',
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+  },
+    body: new FormData(form)
+  }, 
+  )
 }
 
 function sendR(data, evt) {
@@ -17,42 +40,48 @@ function sendR(data, evt) {
 
 async function setReqCur(reqId) {
   const requests = await getReqs();
-  const reqItem = requests.find(item => item.id === reqId)
+  const reqItem = requests.find((item) => item.id === reqId);
+  const reqIdTag = document.getElementById('reqId');
+  reqIdTag.value = reqId;
   const select = document.getElementById('select');
-  select.value = reqItem.method
+  select.value = reqItem.method;
   const urlCur = document.getElementById('url');
-  urlCur.value = reqItem.url
-  const bodyCur = document.getElementById('bodyReq')
-  bodyCur.value = reqItem.body ? JSON.stringify(reqItem.body) : ''
+  urlCur.value = reqItem.url;
+  const bodyCur = document.getElementById('bodyReq');
+  bodyCur.value = reqItem.body ? JSON.stringify(reqItem.body) : '';
+  let headersTag = document.getElementById('headers');
+  headersTag.innerHTML = '';
+  if (reqItem.headers && reqItem.headers.length) {
+    const myHeaders = new Headers(reqItem.headers);
+    myHeaders.forEach((value, key) => {
+      headersTag.innerHTML += `<input id="headerKey" name="headerKey" value='${key}' /><input id="headerValue" name="headerValue" value='${value}' /><button type='button' onclick='deleteHeader({reqId:${reqId},type:"${key}"})'>Удалить</button><br />`;
+    });
+  }
 }
 
-async function sendRequest(form) {  
-  form.action = '/sendReq'
-  form.method = 'POST'
-  console.log('form', form)
+
+async function sendRequest(form) {
+  form.action = '/sendReq';
+  form.method = 'POST';
+  console.log('form', form);
   form.submit();
 }
 
-async function getPage() {
-  // let reqs = document.getElementById('reqs');
-  // const arr = await reqsArr;
-  // arr.forEach((req) => {
-  //   if (req.id) {
-  //     let req_ = JSON.stringify(req)
-  //     reqs.innerHTML += `<button onclick='setReqCur(${req_})' id="${req.id}" class="ta-left w100">Метод: ${req.method}<br />${
-  //       req.url
-  //     }</button>`;
-  //   }
-  // });
-
-  // const form = document.getElementById('form');
-  // form.addEventListener('submit', sendRequest);
-  // async function sendRequest(evt) {
-  //   evt.preventDefault();
-  //   console.log('in sendR******************************************');
-  //   await fetch('/sendReq', {
-  //     method: 'POST',
-  //     body: new FormData(form),
-  //   });
-  // }
+function addHeader() {
+  let headersTag = document.getElementById('headers');
+  headersTag.innerHTML += `<input name='headerType' /><input name='headerValue' /><button type="button" >Удалить</button><br />`;
+}
+async function deleteHeader({ reqId, type }) {
+  const requests = await getReqs();
+  const reqCurrentIndex = requests.findIndex((item) => item.id === reqId);
+  if (reqCurrentIndex >= 0) {
+    const reqCurrent = requests[reqCurrentIndex];
+    const headerForDelIndex = reqCurrent.headers.findIndex(
+      (e) => e[0].toLowerCase() == type
+    );
+    requests[reqCurrentIndex].headers.splice(headerForDelIndex, 1);
+    console.log(200, requests);
+    await saveReqs(requests);
+    setReqCur(reqId)
+  }
 }
