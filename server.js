3c121a11e913acfed8dc8d29166d2c0e5add5bca
galina <galinagatalay@gmail.com>
2024-11-23 18:01:57 +0300

    resolve conflict
    resolve conflict

--- server.js
+++ server.js
@@ -92,7 +92,6 @@ webserver.post('/sendReq', upload.none(), async (req, res) => {
   const response = await fetch(`${body.url}`, {
     method: `${body.method}`,
   });
-  console.log('response.headers', response.headers)
   res.send({
     status: response.status,
     headers: response.headers,
